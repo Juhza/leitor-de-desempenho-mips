89f9b7583e1d1f86c7aa291372ff81495e7508a0
Júlia Beatriz Adão <jubza@edu.univali.br>
2022-09-01 20:50:48 -0300

    add report properties

--- MipsPerformanceReader/Models/PerformanceReport.cs
+++ MipsPerformanceReader/Models/PerformanceReport.cs
@@ -4,12 +4,21 @@ public class PerformanceReport
 {
     public int TotalOfCycles { get; }
     public int TotalOfInstructions { get; }
+    public int TotalOfIType { get; }
+    public int TotalOfRType { get; }
+    public int TotalOfJType { get; }
     public double CyclesPerInstruction { get; }
 
-    public PerformanceReport(int totalOfCycles, int totalOfInstructions)
+    public PerformanceReport(int totalOfCycles, int totalOfInstructions,
+        int totalOfRType, int totalOfJType, int totalOfIType)
     {
         TotalOfCycles = totalOfCycles;
         TotalOfInstructions = totalOfInstructions;
+        
+        TotalOfIType = totalOfIType;
+        TotalOfJType = totalOfJType;
+        TotalOfRType = totalOfRType;
+
         CyclesPerInstruction = totalOfCycles / (double)totalOfInstructions;
     }
 }
--- MipsPerformanceReader/Services/PerformanceReportService.cs
+++ MipsPerformanceReader/Services/PerformanceReportService.cs
@@ -5,12 +5,17 @@ namespace MipsPerformanceReader.Services;
 
 public class PerformanceReportService : IPerformanceReportService
 {
+    private int _totalOfRType = 0;
+    private int _totalOfIType = 0;
+    private int _totalOfJType = 0;
+
     public PerformanceReport GetReport(HexadecimalFile file)
     {
         try
         {
             var content = file.AsBinary();
             var totalOfCycles = 0;
+         
 
             foreach (var binaryValue in content)
             {
@@ -18,7 +23,7 @@ public class PerformanceReportService : IPerformanceReportService
                 totalOfCycles += GetCycleCountByInstructionOpcode(opcode);
             }
 
-            return new PerformanceReport(totalOfCycles, content.Length);
+            return new PerformanceReport(totalOfCycles, content.Length, _totalOfRType, _totalOfJType, _totalOfIType);
         }
         catch (Exception)
         {
@@ -29,22 +34,40 @@ public class PerformanceReportService : IPerformanceReportService
     private int GetCycleCountByInstructionOpcode(string opcode)
     {
         if (TypeOfInstruction.RType.Contains(opcode))
+        {
+            _totalOfRType += 1;
             return CyclesPerTypeOfInstruction.RType;
-        
+        }
+
         if (TypeOfInstruction.Jump.Contains(opcode))
+        {
+            _totalOfJType += 1;
             return CyclesPerTypeOfInstruction.Jump;
+        }
         
         if (TypeOfInstruction.Branch.Contains(opcode))
+        {
+            _totalOfIType += 1;
             return CyclesPerTypeOfInstruction.Branch;
+        }
         
         if (TypeOfInstruction.Load.Contains(opcode))
+        {
+            _totalOfIType += 1;
             return CyclesPerTypeOfInstruction.Load;
+        }
         
         if (TypeOfInstruction.Store.Contains(opcode))
+        {
+            _totalOfIType += 1;
             return CyclesPerTypeOfInstruction.Store;
+        }
         
         if (TypeOfInstruction.IType.Contains(opcode))
+        {
+            _totalOfIType += 1;
             return CyclesPerTypeOfInstruction.IType;
+        }
 
         throw new Exception("missingOpcode");
     }
